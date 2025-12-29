50e8bd5a8f9463c1964d696d1a9eb23c097ce193
Aien Pahlevan <aienpahlevanhim@gmail.com>
2025-12-29 19:14:50 +0330

    Update Home and Header components: add main text to Home, enhance Header layout with responsive search input and new HamburgerMenu icon.

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,34 +1,68 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Bag, SearchNormal1, User } from "iconsax-reactjs";
+import { Bag, HamburgerMenu, SearchNormal1, User } from "iconsax-reactjs";
 import Image from "next/image";
 
 function Header() {
   return (
-    <div className="flex justify-between px-36 pt-10 pb-12">
-      <div className="flex items-center gap-2">
-        <Image alt="کیف ابراشیم" src={"/logo.svg"} width={27} height={34.5} />
-        <div className="text-primary content-end text-2xl leading-2 font-black">
-          کیف ابراشیم
+    <div className="flex flex-col gap-3.5 p-3 px-12 pt-10 md:px-36 md:pb-12">
+      <div className="flex justify-between">
+        <div className="flex items-center gap-2">
+          <Image alt="کیف ابراشیم" src={"/logo.svg"} width={27} height={34.5} />
+          <div className="text-primary content-end text-2xl leading-2 font-black">
+            کیف ابراشیم
+          </div>
         </div>
-      </div>
 
-      <div className="flex gap-3">
-        <div className="relative w-85">
-          <Input placeholder="جست و جو . . ." variant="filled" type="search" />
-          <Button className="absolute top-1/2 left-2 h-8.5 w-8.5 -translate-y-1/2 rounded-2xl bg-white hover:bg-transparent">
-            <SearchNormal1 className="h-5 w-5 text-gray-500" />
+        <div className="flex gap-3">
+          <div className="relative hidden md:block md:w-85">
+            <Input
+              placeholder="جست و جو . . ."
+              variant="filled"
+              type="search"
+              className="h-12.5"
+            />
+            <Button className="absolute top-1/2 left-2 h-8.5 w-8.5 -translate-y-1/2 rounded-2xl bg-white hover:bg-transparent">
+              <SearchNormal1 className="h-5 w-5 text-gray-500" />
+            </Button>
+          </div>
+
+          <Button
+            variant="outline"
+            className="text-primary hidden h-12.5 rounded-2xl md:flex"
+          >
+            <User />
+            <div className="text-[16px] font-semibold">علی ابریشم</div>
+          </Button>
+
+          <Button
+            variant="outline"
+            className="text-primary flex h-12.5 w-12.5 rounded-2xl md:hidden"
+          >
+            <HamburgerMenu />
+          </Button>
+
+          <Button className="h-12.5 w-12.5 rounded-2xl">
+            <Bag variant="Bold" />
           </Button>
         </div>
+      </div>
 
-        <Button variant="outline" className="text-primary rounded-2xl">
-          <User />
-          <div className="text-[16px] font-semibold">علی ابریشم</div>
-        </Button>
-        <Button className="rounded-2xl">
-          <Bag variant="Bold" />
+      <hr className="block text-[#EFEFEF] md:hidden" />
+
+      <div className="relative md:hidden md:w-85">
+        <Input
+          placeholder="جست و جو . . ."
+          variant="filled"
+          type="search"
+          className="h-12.5"
+        />
+        <Button className="absolute top-1/2 left-2 h-8.5 w-8.5 -translate-y-1/2 rounded-2xl bg-white hover:bg-transparent">
+          <SearchNormal1 className="h-5 w-5 text-gray-500" />
         </Button>
       </div>
+
+      <hr className="block text-[#EFEFEF] md:hidden" />
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex h-10 items-center justify-center bg-zinc-50 font-sans"></div>
+    <div className="flex h-10 items-center justify-center bg-zinc-50 font-sans">
+      MAIN
+    </div>
   );
 }
