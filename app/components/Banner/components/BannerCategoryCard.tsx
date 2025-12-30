a3759f8e7dead2a0de7c32b0216aff87c9ff4fe2
Aien Pahlevan <aienpahlevanhim@gmail.com>
2025-12-30 21:31:31 +0330

    --same

--- app/components/Banner/components/BannerCategoryCard.tsx
+++ app/components/Banner/components/BannerCategoryCard.tsx
@@ -19,8 +19,8 @@ function BannerCategoryCard({
       className={`flex items-center justify-between px-4 py-3 md:px-6 md:py-0 ${gradient}`}
     >
       <div className="flex flex-col justify-center gap-2">
-        <div className="text-2xl font-bold">{title}</div>
-        <div className="text-[12px] md:text-sm">{subtitle}</div>
+        <div className="text-2xl font-bold md:text-xl">{title}</div>
+        <div className="text-[12px]">{subtitle}</div>
 
         <Button className="h-8 w-fit rounded-xl bg-[#FFFFFF66] py-0 shadow-[inset_0.6px_0.3px_0_rgba(255,255,255,0.65),0_0_0_0.4px_rgba(255,255,255,0.45)]">
           مشاهده
