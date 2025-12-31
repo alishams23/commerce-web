fb378c52ca84a21d6be568f3d1ecca8c93a1adcf
Aien Pahlevan <aienpahlevanhim@gmail.com>
2025-12-31 12:59:35 +0330

    Add Category component and related assets for product categorization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Banner from "./components/Banner/Banner";
+import Category from "./components/Category/Category";
 import FactoryInfo from "./components/FactoryInfo/FactoryInfo";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-12 md:gap-21">
       <Banner />
-      <div className="md:px-36">
+      <div className="flex flex-col gap-12 md:gap-21 md:px-36">
         <FactoryInfo />
+        <Category />
       </div>
     </div>
   );
