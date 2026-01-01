ac17557e1c7af673a382f0f7cc0e59c56442d97c
Aien Pahlevan <aienpahlevanhim@gmail.com>
2026-01-01 23:10:25 +0330

    Implement Ideal component , responsive.

--- src/app/components/Ideal/Ideal.tsx
+++ src/app/components/Ideal/Ideal.tsx
@@ -1,5 +1,24 @@
+import IdealCard from "./components/IdealCard";
+
 function Ideal() {
-  return <div>Ideal</div>;
+  return (
+    <div className="flex flex-col gap-4 md:flex-row md:gap-6">
+      <IdealCard
+        content="معرفی بهترین کیف های مجموعه"
+        className="bg-[#CCD8FF]"
+        title="ایده آل ها برای شما"
+        titleGradient="from-[#776BB700] via-[#776BB7] to-[#776BB700]"
+        src="/asset/purple-bag.png"
+      />
+      <IdealCard
+        content="بالا ترین کیفیت تولید"
+        className="bg-[#FFF0D3]"
+        title="بهترین ها از آن شماست"
+        titleGradient="from-[#F6E8CC00] via-[#FFC95E] to-[#F6E8CC00]"
+        src="/asset/bag-cat/bag-cat-1.png"
+      />
+    </div>
+  );
 }
 
 export default Ideal;
---
+++ src/app/components/Ideal/components/IdealCard.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+
+import { ArrowLeft } from "iconsax-reactjs";
+
+import { Button } from "@/src/components/ui/button";
+
+type TIdealCardProps = {
+  titleGradient: string;
+  content: string;
+  title: string;
+  className: string;
+  src: string;
+};
+
+function IdealCard({
+  className,
+  title,
+  titleGradient,
+  content,
+  src,
+}: TIdealCardProps) {
+  return (
+    <div
+      className={`flex flex-1 rounded-3xl pt-3.5 pr-4 pb-4 md:pt-6 md:pr-5.5 md:pb-6 ${className}`}
+    >
+      <div className="flex flex-col justify-center md:gap-4">
+        <div
+          className={`bg-linear-to-r text-center text-lg text-white ${titleGradient}`}
+        >
+          {title}
+        </div>
+        <div className="max-w-45.5 text-2xl leading-10 font-black text-[#373254] md:max-w-67.5 md:text-4xl">
+          {content}
+        </div>
+        <Button className="max-w-35 gap-6 rounded-lg border bg-transparent text-[#373254]">
+          <div className="text-[16px] font-semibold">بیشتر</div>
+          <div className="relative z-0 rounded-full border p-1">
+            <ArrowLeft size={13} />
+            <div className="absolute top-0 left-2 -z-10 h-full w-full rounded-full bg-[#FFFFFF4D]" />
+          </div>
+        </Button>
+      </div>
+      <Image
+        alt={title}
+        src={src}
+        width={124}
+        height={124}
+        className="h-31 w-31 self-end md:h-50 md:w-50"
+      />
+    </div>
+  );
+}
+
+export default IdealCard;
