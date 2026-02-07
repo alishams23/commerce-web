95d78409e0f38e186ee911733c05d287a454e8f6
Aien Pahlevan <aienpahlevanhim@gmail.com>
2026-02-07 21:05:57 +0330

    refactor: simplify AmazingDiscounts and AmazingDiscountsTimer components; enhance Footer scroll functionality; update Logo cursor style

--- src/app/components/Header/components/Logo.tsx
+++ src/app/components/Header/components/Logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function Logo() {
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex cursor-default items-center gap-2">
       <Image alt="کیف ابراشیم" src="/logo.svg" width={27} height={34.5} />
       <div className="text-primary content-end font-[Kalameh] text-2xl leading-2 font-black">
         کیف ابراشیم
