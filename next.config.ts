e52cb1f50f54fc313f05c41326678b53516c99bb
Aien Pahlevan <aienpahlevanhim@gmail.com>
2026-02-17 13:52:08 +0330

    feat: update image remotePatterns configuration to use dynamic protocol and hostname

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,18 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
-      new URL(`${process.env.NEXT_PUBLIC_API_BASE_URL}/media/**`),
+      // new URL(`${process.env.NEXT_PUBLIC_API_BASE_URL}/media/**`),
+      {
+        protocol: process.env.NEXT_PUBLIC_API_BASE_URL?.startsWith("https")
+          ? "https"
+          : "http",
+        hostname:
+          process.env.NEXT_PUBLIC_API_BASE_URL?.replace(
+            /https?:\/\//,
+            "",
+          ).replace(/:\d{4}/, "") || "",
+        pathname: "/media/**",
+      },
     ],
   },
 };
