bbe5958802888b9b1ea85356c5e28bdd32f6c198
Aien Pahlevan <aienpahlevanhim@gmail.com>
2026-02-17 15:08:24 +0330

    Feat: Implement Aparat Ads videos

--- src/components/FactoryInfo/components/FactoryInfoMedia.tsx
+++ src/components/FactoryInfo/components/FactoryInfoMedia.tsx
@@ -1,5 +1,13 @@
 function FactoryInfoMedia() {
-  return <div className="bg-primary h-74.5 rounded-[28px]" />;
+  return (
+    <div className="overflow-hidden rounded-[28px] lg:h-74.5">
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://www.aparat.com/video/video/embed/videohash/6NGlq/vt/frame"
+      ></iframe>
+    </div>
+  );
 }
 
 export default FactoryInfoMedia;
